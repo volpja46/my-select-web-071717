b03222db89472bf7aa354b717267be180c58b941
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-11 21:39:45 -0400

    finished

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
-end
+     i = 0
+      new_array = []
+     if collection.length > 0
+      while i < collection.length
+       if yield(collection[i])
+      new_array << collection[i]
+     end
+       i+=1
+     end
+   else
+    puts "Empty collection"
+   end
+   new_array
+    end
